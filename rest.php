d3f35a37928b028f4b69713382acc313720efcf1
yianis <djazzmusic@gmail.com>
2016-04-08 15:37:13 +0300

    small comments

--- rest.php
+++ rest.php
@@ -1,13 +1,10 @@
 <?php
 // Initial variables
-
-
 $ip=$_GET['ip'];
-
 $iplong=ip2long($ip); // change the ip to a number.
 
 
-if (is_numeric($iplong)) // this would make sure that no XSS attacks or any dirty input will pass and out api is a bit more safe
+if (is_numeric($iplong)) // this would make sure that no XSS attacks or any dirty input will pass and out api is a bit more safe. 
 {
 
 
@@ -23,7 +20,6 @@ Since all the ip are in a numeric format, finding the subnet that the ip belongs
 */
 
 $result = mysqli_query($connection,"SELECT *  FROM ipgeo USE INDEX (a,b) WHERE `a` <= $iplong AND `b` >= $iplong LIMIT 0,1");
-
   
 while($row = mysqli_fetch_array($result))
 {
@@ -32,11 +28,11 @@ while($row = mysqli_fetch_array($result))
 	$data[]=$row;
 }
 	if(isset($data))
-			{ // create a json 
+			{ // create a json output for our API.
 			echo json_encode($data);
 			}
 		else
-			{ //case that we didn't find a record on the database.
+			{ // In case that we didn't find a record on the database.
 				$error['error']='No record has been found';
 				$error['status']='0';
 				echo json_encode($error);
