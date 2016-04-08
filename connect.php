2b09dc4d819320747fa5d88267a12792000edca0
yianis <djazzmusic@gmail.com>
2016-04-08 04:51:21 +0300

    small fix

--- connect.php
+++ connect.php
@@ -1,19 +1,15 @@
 <?php
 
 
-
 // --- DATABASE CONNECT
 				$username="pure360";
 				$password="Pure360";
 				$database="pure360";
-		//	$server="192.168.1.9";
-				$server="192.168.5.2";
-		//		$server="localhost";
-				$site_is_online=0;
+			$server="192.168.5.2";
+					$site_is_online=0;
 				$sql_port="3306";
 				
 					$connection = mysqli_connect($server,$username,$password,$database,$sql_port);
-	$ip_rem=$_SERVER["REMOTE_ADDR"] . " " . $_SERVER['HTTP_USER_AGENT'] . " " . $_SERVER['REQUEST_URI'];
 	
 	if(!isset($no_out_put))
 	{
