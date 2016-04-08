b0f4a32921b48d6aafd26de82aacd6606dbb5ab4
yianis <djazzmusic@gmail.com>
2016-04-08 03:21:50 +0300

    query with indexes and small fixes

--- rest.php
+++ rest.php
@@ -1,23 +1,28 @@
 <?php
 // Initial variables
 
-ini_set('memory_limit', '564M');
-
-// --- DATABASE CONNECT
-$no_out_put=1;
-include('connect.php');
 
 $ip=$_GET['ip'];
 
 $iplong=ip2long($ip);	
 
 
+if (is_numeric($iplong)) // this would make sure that no XSS attacks or any dirty input will pass and out api is a bit more safe
+{
+
+
+// --- DATABASE CONNECT
+$no_out_put=1; // dont show any output from connect.php
+include('connect.php');
+
+
 // ----------------------------- READ FROM DATABASE
 
-//echo "$ipfirst-$iplast";
+/*
+Since all the ip are in a numeric format, finding the subnet that the ip belongs is a simple maths problem. 
+*/
 
-
-$result = mysqli_query($connection,"SELECT *  FROM ipgeo WHERE `a` <= $iplong AND `b` >= $iplong LIMIT 0,10");
+$result = mysqli_query($connection,"SELECT *  FROM ipgeo USE INDEX (a,b) WHERE `a` <= $iplong AND `b` >= $iplong LIMIT 0,1");
 
  
   
@@ -27,8 +32,21 @@ while($row = mysqli_fetch_array($result))
 //	print_r($row);
 	$data[]=$row;
 }
-
+if(isset($data))
+{ // create a json 
 echo json_encode($data);
+}
+else
+{ //case that we didn't find a record on the database.
+	$error['error']='No record has been found';
+	$error['status']='0';
+	echo json_encode($error);
+}
 
+}
+else
+{
+	echo "Wrong ip entered";
+}
 
 ?>
